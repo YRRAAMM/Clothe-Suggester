ef8b1cfc62ff3fd5c79482c563c5523576429f0b
youssef <yr925344git@gmail.com>
2023-04-16 23:01:54 +0200

    init project

---
+++ app/src/main/java/com/example/weatherclothet/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weatherclothet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// 1c14129ac4a67fe22ff41623a3565598
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
